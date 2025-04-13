5ef232f28794cbc2b2543bfcec5c1efbd6580b3a
akshayvp1 <akshayachu2427@gmail.com>
2025-04-13 12:54:17 +0530

    Updated project structure and assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Login from './Page/user/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './Page/user/Home/Home';
-import UserProfile from './Page/user/Profile/Profile';
-import ProtectedRoute from './utils/ProtectedRoute';
+// import Dashboard from './Page/user/Home/Home';
+// import UserProfile from './Page/user/Profile/Profile';
+// import ProtectedRoute from './utils/ProtectedRoute';
 import { ToastContainer } from 'react-toastify'; 
 import 'react-toastify/dist/ReactToastify.css'; 
-import AdiminLogin from './Page/admin/Login/Login'
-import AdminDashbord from './Page/admin/Home/Home'
-import AddUser from './Page/admin/AddUser/AddUser'
+// import AdiminLogin from './Page/admin/Login/Login'
+// import AdminDashbord from './Page/admin/Home/Home'
+// import AddUser from './Page/admin/AddUser/AddUser'
 
 
 function App() {
@@ -18,12 +18,12 @@ function App() {
         
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<AdiminLogin />} />
+        {/* <Route path="/admin" element={<AdiminLogin />} />
         <Route path="/admin-dashbord" element={<ProtectedRoute><AdminDashbord /></ProtectedRoute>} />
         <Route path="/add-user" element={<ProtectedRoute><AddUser /></ProtectedRoute>} />
         
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="/profile" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} /> */}
         
       </Routes>
       <ToastContainer />
--- client/src/Page/user/Login/Login.jsx
+++ client/src/Page/user/Login/Login.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import LoginUser from '../../../Components/User/Login/Login'
-import Footer from '../../../Components/User/Footer/Footer'
-import NavBar from '../../../Components/User/NavBar/NavBar'
+
 function Login() {
   return (
     <div>
-        <NavBar/>
+        
         <LoginUser/>
-        <Footer/>
+        
     </div>
   )
 }
